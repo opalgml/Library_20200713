07b4e638d8802b2e72d62356ebd6853f75cdf19a
김미희 <opalgml@naver.com>
2020-07-13 20:16:14 +0900

    사진 크게보기 화면 진입   => 확대 / 축소 지원 안됨

--- app/src/main/java/kr/co/tjoeun/library_20200713/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/library_20200713/MainActivity.kt
@@ -1,7 +1,9 @@
 package kr.co.tjoeun.library_20200713
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
 
@@ -12,6 +14,10 @@ class MainActivity : BaseActivity() {
         setValues()
         setupEvents()
 
+        goToPhotoViewBtn.setOnClickListener {
+            val myIntent = Intent(mContext, ProfilePhotoActivity::class.java)
+            startActivity(myIntent)
+        }
     }
 
     override fun setValues() {
---
+++ app/src/main/java/kr/co/tjoeun/library_20200713/ProfilePhotoActivity.kt
@@ -0,0 +1,20 @@
+package kr.co.tjoeun.library_20200713
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfilePhotoActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_photo)
+    }
+
+    override fun setValues() {
+
+    }
+
+    override fun setupEvents() {
+
+    }
+}
