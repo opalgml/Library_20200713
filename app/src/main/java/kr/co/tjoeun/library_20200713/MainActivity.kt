fa656566ecc0bff642512f8e17a1eced1cad462a
김미희 <opalgml@naver.com>
2020-07-13 20:53:47 +0900

    Glide 라이브러리 사용 예시   - 아직 이미지 표시는 X

--- app/src/main/java/kr/co/tjoeun/library_20200713/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/library_20200713/MainActivity.kt
@@ -3,7 +3,9 @@ package kr.co.tjoeun.library_20200713
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_profile_photo.*
 
 class MainActivity : BaseActivity() {
 
@@ -21,6 +23,9 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        val imgUrl = "https://lh3.googleusercontent.com/proxy/zohuDN_imkYt9xNIGRkdRfiZLH6hUer1r3Z6s3wjj3vtgMIhe61iwzJA0f3jPjGIViapUVHKM1nWsHXw_etzgIpJzvrIyVosSLxacmKVLU_qpYNFiHAriYRABaLGRo4"
+        Glide.with(mContext).load(imgUrl).into(profileImg)
 
     }
 
