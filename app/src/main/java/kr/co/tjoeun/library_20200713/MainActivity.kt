b06920311e1674bbcfbda4eda3e068f9355b8784
김미희 <opalgml@naver.com>
2020-07-14 19:38:19 +0900

    라이브러리를 이용한 실제 통화 기능   => 권한 확인까지 같이 진행

--- app/src/main/java/kr/co/tjoeun/library_20200713/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/library_20200713/MainActivity.kt
@@ -1,10 +1,14 @@
 package kr.co.tjoeun.library_20200713
 
+import android.Manifest
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.bumptech.glide.Glide
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
@@ -33,10 +37,31 @@ class MainActivity : BaseActivity() {
 
         callBtn.setOnClickListener {
 
-            val phoneNum = phoneNumTxt.toString()
-            val myUri = Uri.parse("tel:${phoneNum}")
-            val myIntent = Intent(Intent.ACTION_CALL, myUri)
-            startActivity(myIntent)
+//            TedPermission 을 이용해서 통화 권한을 허락할 것인지 질문!
+            val permissionListener = object : PermissionListener{
+//                승인을 받은 상황 -> 전화 ok
+                override fun onPermissionGranted() {
+                    val phoneNum = phoneNumTxt.toString()
+                    val myUri = Uri.parse("tel:${phoneNum}")
+                    val myIntent = Intent(Intent.ACTION_CALL, myUri)
+                    startActivity(myIntent)
+                }
+
+//                최종 거부된 상황 -> 전화 x
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+
+                    Toast.makeText(mContext, "통화 권한이 거부되어 통화 연결이 불가능합니다.", Toast.LENGTH_SHORT)
+
+                }
+
+            }
+
+//            실제 권한 확인 요청
+            TedPermission.with(mContext) //with -> 어느 화면에서 권한을 확인할 것인지
+                .setPermissions(Manifest.permission.CALL_PHONE)
+                .setDeniedMessage("설정 에서 통화 권한을 허용해주어야 합니다.")
+                .setPermissionListener(permissionListener)
+                .check()
 
         }
     }
